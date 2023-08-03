0cc035f6c638ddceda490e5205b3577070f2f821
noor.alqutob01 <d62brduxmrdsgrxa7wqxoexya6jmcg5iymni6p2heh4l3zw5occa>
2023-08-03 10:14:43 +0300

    css tiny fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "@/styles/Home.module.css";
 import Search from "./Components/Search/Search";
 import { Raleway } from "next/font/google";
